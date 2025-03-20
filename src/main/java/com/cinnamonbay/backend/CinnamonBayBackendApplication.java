6cb001dc70635112d39e7bc2ff2f0f550d6bccad
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-03-20 12:16:18 +0530

    Initial commit for cinnamon-bay-backend

---
+++ src/main/java/com/cinnamonbay/backend/CinnamonBayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinnamonbay.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnamonBayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinnamonBayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinnamonbay/backend/CinnamonBayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinnamonbay.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinnamonBayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
