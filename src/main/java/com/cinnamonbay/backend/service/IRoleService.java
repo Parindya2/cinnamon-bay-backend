080eb89c3f4f257634ba5b0a4d511557c38140b6
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-07-24 23:22:46 +0530

    feat(service): add IRoleService and IRoomService interfaces with method definitions

--- src/main/java/com/cinnamonbay/backend/service/IRoleService.java
+++ src/main/java/com/cinnamonbay/backend/service/IRoleService.java
@@ -1,4 +1,21 @@
 package com.cinnamonbay.backend.service;
 
+import com.cinnamonbay.backend.model.*;
+
+import java.util.*;
+
 public interface IRoleService {
+    List<Role> getRoles();
+
+    Role createRole(Role theRole);
+
+    void deleteRole(Long id);
+
+    Role findByName(String name);
+
+    User removeUserFromRole(Long userId, Long roleId);
+
+    User assignRoleToUser(Long userId, Long roleId);
+
+    Role removeAllUsersFromRole(Long roleId);
 }
