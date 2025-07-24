4363a0e07b41e4c1a3b94e98d761e3b5d42f1b9a
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-07-24 23:18:56 +0530

    feat(service): implement UserService with user registration, retrieval, and deletion logic

--- src/main/java/com/cinnamonbay/backend/service/UserService.java
+++ src/main/java/com/cinnamonbay/backend/service/UserService.java
@@ -1,4 +1,59 @@
 package com.cinnamonbay.backend.service;
 
-public class UserService {
+import com.cinnamonbay.backend.exception.*;
+import com.cinnamonbay.backend.model.*;
+import com.cinnamonbay.backend.model.User;
+import com.cinnamonbay.backend.repository.*;
+import lombok.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new UserAlreadyExistsException(user.getEmail() + " already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Find or create default role
+        Role userRole = roleRepository.findByName("ROLE_USER")
+                .orElseGet(() -> {
+                    Role newRole = new Role("ROLE_USER");
+                    return roleRepository.save(newRole);
+                });
+
+        user.setRoles(Collections.singletonList(userRole));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(String email) {
+        User theUser = getUser(email);
+        if (theUser != null) {
+            userRepository.deleteByEmail(email);
+        }
+    }
+
+    @Override
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
