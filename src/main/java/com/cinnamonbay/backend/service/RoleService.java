7aa693007ce2a4809d4a0a0f321e64f2260f954f
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-07-24 23:19:40 +0530

    feat(service): implement RoleService with role management and user-role assignment logic

--- src/main/java/com/cinnamonbay/backend/service/RoleService.java
+++ src/main/java/com/cinnamonbay/backend/service/RoleService.java
@@ -1,4 +1,85 @@
 package com.cinnamonbay.backend.service;
 
-public class RoleService {
+import com.cinnamonbay.backend.exception.*;
+import com.cinnamonbay.backend.model.*;
+import com.cinnamonbay.backend.model.User;
+import com.cinnamonbay.backend.repository.*;
+import lombok.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService implements IRoleService {
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Role createRole(Role theRole) {
+        String roleName = "ROLE_"+theRole.getName().toUpperCase();
+        Role role = new Role(roleName);
+        if (roleRepository.existsByName(roleName)){
+            throw new RoleAlreadyExistException(theRole.getName()+" role already exists");
+        }
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void deleteRole(Long roleId) {
+        this.removeAllUsersFromRole(roleId);
+        roleRepository.deleteById(roleId);
+    }
+
+    @Override
+    public Role findByName(String name) {
+        return roleRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Role not found: " + name));
+    }
+
+    @Override
+    public User removeUserFromRole(Long userId, Long roleId) {
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role> role = roleRepository.findById(roleId);
+
+        if (user.isPresent() && role.isPresent() && role.get().getUsers().contains(user.get())){
+            role.get().removeUserFromRole(user.get());
+            roleRepository.save(role.get());
+            return user.get();
+        }
+        throw new UsernameNotFoundException("User not found or not assigned to this role");
+    }
+
+    @Override
+    public User assignRoleToUser(Long userId, Long roleId) {
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role> role = roleRepository.findById(roleId);
+
+        if (user.isPresent() && role.isPresent()) {
+            if (user.get().getRoles().contains(role.get())){
+                throw new UserAlreadyExistsException(
+                        user.get().getFirstName()+ " is already assigned to the " + role.get().getName()+ " role");
+            }
+            role.get().assignRoleToUser(user.get());
+            roleRepository.save(role.get());
+            return user.get();
+        }
+        throw new UsernameNotFoundException("User or Role not found");
+    }
+
+    @Override
+    public Role removeAllUsersFromRole(Long roleId) {
+        Optional<Role> role = roleRepository.findById(roleId);
+        if (role.isPresent()) {
+            role.get().removeAllUsersFromRole();
+            return roleRepository.save(role.get());
+        }
+        throw new RuntimeException("Role not found with id: " + roleId);
+    }
 }
