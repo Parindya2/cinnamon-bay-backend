be23ef3f70905d9ef2baec8d77704063f86265f5
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-08-09 22:03:46 +0530

    feat(service): define IUserService interface for user management operations

--- src/main/java/com/cinnamonbay/backend/service/IUserService.java
+++ src/main/java/com/cinnamonbay/backend/service/IUserService.java
@@ -1,4 +1,15 @@
 package com.cinnamonbay.backend.service;
 
+import com.cinnamonbay.backend.model.*;
+
+import java.util.*;
+
 public interface IUserService {
+    User registerUser(User user);
+
+    List<User> getUsers();
+
+    void deleteUser(String email);
+
+    User getUser(String email);
 }
